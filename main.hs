de69c2e7702ab3834c81b5f6737e7535342cf718
Georgi Khomeriki <g.khomeriki@gmail.com>
2016-01-04 09:06:23 +0000

    code organisation

--- main.hs
+++ main.hs
@@ -5,7 +5,9 @@ import Control.Monad
 import Data.IORef
 import Data.Maybe
 
-newtype Observable a = Observable { subscribe :: Observer a -> IO () }
+-------------------- TYPES ----------------------
+
+newtype Observable a = Observable { onSubscribe :: Observer a -> IO () }
 
 data Observer a = Observer { onNext :: a -> IO (), 
                              onError :: SomeException -> IO (),
@@ -14,6 +16,8 @@ data Observer a = Observer { onNext :: a -> IO (),
                              subscription :: IO Subscription }
 
 type Subscription = IORef (Maybe [IO ()])
+
+---------------- INSTANCE DECLARATIONS -----------------
 
 instance Functor Observable where
   fmap f = lift (coMap f)
@@ -30,27 +34,34 @@ instance Applicative Observable where
 instance Monad Observable where
   o >>= f = rxMerge $ fmap f o
 
+--------------- GENERAL FUNCTIONS -----------------
+
 createObserver :: (a -> IO ()) -> (SomeException -> IO ()) -> IO () -> Observer a
 createObserver onNext onError onCompleted = Observer onNext onError onCompleted addSubscription subscription
   where
     subscription = newIORef $ Just []
     addSubscription action = do s <- subscription
                                 actions <- readIORef s
-                                when (isJust actions) . writeIORef s $ fmap (action :) actions
+                                when (isJust actions) . writeIORef s $ fmap (action :) actions 
 
-lift :: (Observer b -> Observer a) -> Observable a -> Observable b
-lift f oa = Observable (subscribe oa . f)
-
-isUnsubscribed :: Subscription -> IO Bool
-isUnsubscribed s = isNothing <$> readIORef s
+subscribe :: Observable a -> Observer a -> IO Subscription
+subscribe obl obr = onSubscribe obl obr >> subscription obr
 
 unsubscribe :: Subscription -> IO ()
 unsubscribe s = do subs <- readIORef s
                    when (isJust subs) $ sequence_ (fromJust subs) >> writeIORef s Nothing
 
+isUnsubscribed :: Subscription -> IO Bool
+isUnsubscribed s = isNothing <$> readIORef s
+
+-------------------- OPERATORS --------------------
+
+lift :: (Observer b -> Observer a) -> Observable a -> Observable b
+lift f oa = Observable (onSubscribe oa . f)
+
 -- TODO: `onCompleted obr` is never called now
 rxMerge :: Observable (Observable a) -> Observable a
-rxMerge oas = Observable (\obr -> subscribe oas $ createObserver (\oa -> subscribe oa $ createObserver (onNext obr) (onError obr) (return ())) (onError obr) (return ()))
+rxMerge oas = Observable (\obr -> onSubscribe oas $ createObserver (\oa -> onSubscribe oa $ createObserver (onNext obr) (onError obr) (return ())) (onError obr) (return ()))
 
 rxCombineLatestWith :: (a -> b -> c) -> Observable a -> Observable b -> Observable c
 rxCombineLatestWith f oa ob = uncurry f <$> rxCombineLatest oa ob
@@ -60,8 +71,8 @@ rxCombineLatest oa ob = Observable (\obr -> do refA <- newIORef Nothing
                                                refB <- newIORef Nothing
                                                doneA <- newIORef False
                                                doneB <- newIORef False
-                                               subscribe oa $ createObserver (onNextA refA refB obr) (onError obr) (handleOnCompleted doneB doneA obr)
-                                               subscribe ob $ createObserver (onNextB refA refB obr) (onError obr) (handleOnCompleted doneA doneB obr))
+                                               onSubscribe oa $ createObserver (onNextA refA refB obr) (onError obr) (handleOnCompleted doneB doneA obr)
+                                               onSubscribe ob $ createObserver (onNextB refA refB obr) (onError obr) (handleOnCompleted doneA doneB obr))
   where
     handleOnCompleted readRef writeRef obr = do done <- readIORef readRef 
                                                 if done then onCompleted obr else writeIORef writeRef True
@@ -71,6 +82,7 @@ rxCombineLatest oa ob = Observable (\obr -> do refA <- newIORef Nothing
     onNextB refA refB obr b = do maybeA <- combine refA refB b
                                  when (isJust maybeA) $ onNext obr (fromJust maybeA, b)
 
+-- or just use mfilter (MonadPlus)?
 rxFilter :: (a -> Bool) -> Observable a -> Observable a
 rxFilter p = lift (coFilter p)
   where
@@ -78,6 +90,8 @@ rxFilter p = lift (coFilter p)
     coFilter p oa = createObserver (\a -> when (p a) $ onNext oa a)
                                    (onError oa)
                                    (onCompleted oa)
+
+------------------------------ TEST -------------------------------
 
 stream :: Observable Integer
 stream = Observable (\observer -> do onNext observer 1
